4f30ca9fbc920bf502eb1d2f5f688bae33bc6fe9
Rajdeep2922 <dk708362@gmail.com>
2026-02-26 23:28:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express from "express";  
-import dotenv from "dotenv/config";  
-import cors from "cors";  
-import http from "http";  
+import express from "express";
+import dotenv from "dotenv/config";
+import cors from "cors";
+import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoute.js";
@@ -14,23 +14,23 @@ const app = express();
 const server = http.createServer(app);
 
 //Initialize socket
-export const io = new Server(server,{
-    cors:{origin:"*"}
+export const io = new Server(server, {
+    cors: { origin: "*" }
 })
 //store online users
 export const userSocketMap = {};//{userId:socketId}
 
 //socket.io connection handler
-io.on("connection", (socket)=>{
+io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     console.log("User connected: ", userId);
 
-    if(userId) userSocketMap[userId] = socket.id;
+    if (userId) userSocketMap[userId] = socket.id;
 
     //emit online users to all connected clients
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect",()=>{
+    socket.on("disconnect", () => {
         console.log("User Disconnected", userId);
         delete userSocketMap[userId];
         io.emit("getOnlineUsers", Object.keys(userSocketMap))
@@ -39,11 +39,11 @@ io.on("connection", (socket)=>{
 
 
 // Middleware
-app.use(express.json({limit :"4mb"}))
+app.use(express.json({ limit: "4mb" }))
 app.use(cors());
 
 //Routes setup
-app.use("/api/status", (req,res)=> res.send("Server is live"))
+app.use("/api/status", (req, res) => res.send("Server is live"))
 app.use("/api/auth", userRouter)
 app.use("/api/messages", messageRouter)
 
@@ -51,10 +51,8 @@ app.use("/api/messages", messageRouter)
 await connectDB();
 
 
-if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT));
-}
+server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
 
 //export server for vercel
 export default server;
